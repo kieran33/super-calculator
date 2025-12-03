4e02281eae356d186ffc56ccca965c4d8bf37138
Kiéran <lavole.kieran@gmail.com>
2025-12-03 16:51:54 +0100

    ajout fichier

--- super-calculator/src/App.jsx
+++ super-calculator/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Money from "./components/money";
+import Chart from "./components/chart";
 
 function App() {
   return (
     <div>
       <h1>test</h1>
       <Money />
+      <Chart />
     </div>
   );
 }
